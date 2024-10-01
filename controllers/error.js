cadd89d24e2e141aa05c344861685912ebec99d3
DekunleJr <adekunlesa10@gmail.com>
2024-10-01 12:21:51 +0100

    break

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,3 @@
 exports.get404 = (req, res, next) => {
-    res.status(404).render('error', {pageTitle: 'Page Not found'})
+    res.status(404).render('error', {pageTitle: 'Page Not found', path: '/'})
 };
