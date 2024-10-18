0962f18a0a3795ea7eef9d10fcb86079067eba5a
DekunleJr <adekunlesa10@gmail.com>
2024-10-18 12:47:28 +0100

    Mysql database add

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const errorController = require('./controllers/error.js');
+const db = require('./util/database.js');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
+
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Sa@726835'
+});
+
+module.exports = pool.promise();
